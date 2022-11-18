5b6f52f850a6688560c854e6b6aca451ba9299d7
Islomjon Makhamadjanov <islomiy1101@gmail.com>
2022-11-18 11:44:09 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -148,8 +148,10 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.normpath(os.path.join(BASE_DIR,'static'))
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT=BASE_DIR/'static'
+STATICFILES_DIRS=[
+    STATIC_ROOT
+]
 # STATIC_ROOT=os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
