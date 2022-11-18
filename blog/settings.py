01d41c52e54f025a882ea2306a44da62b5651b3a
Islomjon Makhamadjanov <islomiy1101@gmail.com>
2022-11-18 11:48:26 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -148,7 +148,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR/'static'
+#STATIC_ROOT=BASE_DIR/'static'
 STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
