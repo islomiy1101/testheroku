b9add3a0531e32049514884cd388153fb20401be
Islomjon Makhamadjanov <islomiy1101@gmail.com>
2022-11-18 11:52:50 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -149,10 +149,8 @@ LOCALE_PATHS = [
 import os
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATIC_ROOT=os.path.join(BASE_DIR,'static')
